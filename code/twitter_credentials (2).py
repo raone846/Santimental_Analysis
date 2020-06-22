bcfa5ed188e139e5f628943e2b09823b683f8341
raone846 <rohanpandey846@gmail.com>
2020-06-22 10:27:30 +0530

    final

---
+++ code/twitter_credentials (2).py
@@ -0,0 +1,5 @@
+# Variables that conatain the user credentials to access Twitter API.
+ACCESS_TOKEN = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+ACCESS_TOKEN_SECRET = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_KEY = "xxxxxxxxxxxxxxxxxxxxxxxxxxx"
+CONSUMER_SECRET= "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx"
